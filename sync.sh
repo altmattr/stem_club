cef3d96e597f9b2c30fcdc787942cf87344b0fec
Jasper Mason Catley <62457579+mqjasper@users.noreply.github.com>
2023-10-07 14:12:09 +1100

    Update sync.sh
    Added --mkpath to permit the server to create new directories when sending data

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 SN=$(cat /sys/firmware/devicetree/base/serial-number)
 echo $SN
-rsync -avz logs/ datakeeper@10.0.2.175:logs/$SN/
+rsync -avz --mkpath logs/ datakeeper@10.0.2.175:logs/$SN/
