a5d0adc0009ab211127b1f5f15c9e0794ebf9095
mqjasper <jasper.catley@students.mq.edu.au>
2023-11-20 15:14:51 +1100

    No more rotations

--- capture.sh
+++ capture.sh
@@ -4,4 +4,4 @@ echo "Any existing images not saved from the folder will be overwritten"
 export LIBCAMERA_LOG_LEVELS=*:4
 libcamera-still -n -t 0 --timelapse 1000 -o /home/pi/stem_club/captures/img%04d.jpg
 echo "Rotating captures... Please wait"
-for f in /home/pi/stem_club/captures/*.jpg; do convert "$f" -rotate 270 "/home/pi/stem_club/captures/${f%.jpg}.jpg"; done
+#for f in /home/pi/stem_club/captures/*.jpg; do convert "$f" -rotate 270 "/home/pi/stem_club/captures/${f%.jpg}.jpg"; done
