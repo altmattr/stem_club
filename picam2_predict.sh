9374504078c26c303e798471fdc946add447a0c9
Matt Roberts <matthew.roberts@mq.edu.au>
2023-12-19 23:35:41 +0000

    adding zero to 9 model

--- picam2_predict.sh
+++ picam2_predict.sh
@@ -55,6 +55,7 @@ models = [("Stem Club", "models/stem_club"),
 		("Inception V4", "models/inception_v4_quant"),
 		("Is the camera covered?", "models/covered_float"),
 		("Numbers 0 to 5", "models/Zero_Five_Model03"),
+		("Numbers 0 to 9", "models/zero_nine"),
 		("Glasses or not glasses?", "models/glasses_or_not")
 		]
 
