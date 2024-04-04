2a9dd63832500e424d736c935fc30df41e1fc69c
mqjasper <jasper.catley@students.mq.edu.au>
2024-04-04 12:13:36 +1100

    Move from libcamera-apps to rpicam-apps

--- setup.sh
+++ setup.sh
@@ -18,6 +18,7 @@ esac
 #sudo apt-get install -y python3-opencv
 #sudo apt-get install -y network-manager dnsmasq
 #sudo apt-get install -y sshpass
+#sudo apt-get install -y rpicam-apps
 
 #sudo systemctl disable dnsmasq
 #sudo systemctl enable NetworkManager
