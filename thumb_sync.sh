eff6c7d64d0875fa004d158e27d13ebef55b707a
mqjasper <jasper.catley@students.mq.edu.au>
2024-06-20 15:16:01 +1000

    Updated thumb_sync script to load files onto the first partition of the first mass storage drive connected to pi. Added correct method of adjusting libcamera log level to happy_snaps script, although it is not necessary when using prebuilt image.

--- thumb_sync.sh
+++ thumb_sync.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir /media/thumb
-sudo chown -R pi /media/thumb
-drive=`lsblk | grep -m 1 -o "sd[a|b|c]"`
+sudo chown -R pi /mnt/transfer
+drive=sda1
 echo $drive
-sudo mount -o rw,uid=pi /dev/$drive /media/thumb
-mkdir /media/thumb/happy_snaps
-mkdir /media/thumb/captures
-mv /home/pi/stem_club/happy_snaps/* /media/thumb/happy_snaps/.
-mv /home/pi/stem_club/captures/* /media/thumb/captures/.
-sudo umount /media/thumb
+sudo mount -o rw,uid=pi /dev/$drive /mnt/transfer
+mkdir /mnt/transfer/happy_snaps
+mkdir /mnt/transfer/captures
+mv /home/pi/stem_club/happy_snaps/* /mnt/transfer/happy_snaps/.
+mv /home/pi/stem_club/captures/* /mnt/transfer/captures/.
+sudo umount /mnt/transfer
+echo "Transfer complete, drive unmounted"
